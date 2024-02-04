610272e3ec0fa5b0eb04f6949f510faf57719e1b
zhangzhongyou <zhangzhongyou@bytedance.com>
2024-02-04 21:35:16 +0800

    feat: 更新页面

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -20,7 +20,7 @@ const Login = () => {
     }
   };
   return (
-    <div className="bg-[#131416] h-screen w-screen flex items-center justify-center flex-col relative">
+    <div className="bg-[#131416] h-screen w-screen flex items-center justify-center flex-col relative scope">
       <img src={yellow} className="absolute top-0 left-0 z-[2] w-[430px] h-[255px]" onClick={handleGotoRegister} />
       <img src={blue} className="absolute top-0 left-0 z-[1] w-[580px] h-[215px]" />
       <div className="absolute top-0 left-0 z-[3]">
